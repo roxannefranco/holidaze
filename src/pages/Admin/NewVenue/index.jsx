4ab641f6111e597085e40f6b95a5843f3beaa2e5
roxannefranco <roxannefranco10@gmail.com>
2023-09-16 17:08:35 +0200

    New venue form

--- src/pages/Admin/NewVenue/index.jsx
+++ src/pages/Admin/NewVenue/index.jsx
@@ -1,12 +1,46 @@
 import styles from "./styles.module.css";
 import Layout from "../../../components/Layout";
 import Header from "../../../components/Header";
+import Input from "../../../components/Input";
+import Checkbox from "../../../components/Checkbox";
+import Button from "../../../components/Button";
 
 function NewVenue() {
   return (
     <Layout>
       <Header></Header>
-      <h1>hello new venue page</h1>
+      <div>
+        <h1 className={styles.mainTitle}>New Venue</h1>
+        <form>
+          <Input type="text" label="Name" />
+          <Input type="text" label="Description" />
+          <Input type="text" label="Media" placeholder="URL" />
+          <Input type="text" label="Price" placeholder="kr" />
+          <Input type="text" label="Max Guests" />
+          <div className={styles.amenitiesBox}>
+            <span>Amenities</span>
+            <Checkbox label="Wifi"></Checkbox>
+            <Checkbox label="Parking"></Checkbox>
+            <Checkbox label="Breakfast"></Checkbox>
+            <Checkbox label="Pets"></Checkbox>
+          </div>
+          <span>Location</span>
+          <Input type="text" label="Address" />
+          <Input type="text" label="City" />
+          <Input type="text" label="Zip" />
+          <Input type="text" label="Country" />
+          <Input type="text" label="Latitude" />
+          <Input type="text" label="Longitude" />
+          <div className={styles.btns}>
+            <Button size="lg" type="primary">
+              Create venue
+            </Button>
+            <Button size="lg" type="secondary">
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 }
--- src/pages/Admin/Venues/index.jsx
+++ src/pages/Admin/Venues/index.jsx
@@ -1,8 +1,10 @@
 import styles from "./styles.module.css";
+import Header from "../../../components/Header";
 
 function Venues() {
   return (
     <main>
+      <Header></Header>
       <h1>Venues</h1>
     </main>
   );
