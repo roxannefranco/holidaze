60faff398d925c3d3b111a713ccd8c477809b473
roxannefranco <roxannefranco10@gmail.com>
2023-09-18 22:37:46 +0200

    Create venue form stylling

--- src/pages/Admin/NewVenue/index.jsx
+++ src/pages/Admin/NewVenue/index.jsx
@@ -9,22 +9,29 @@ function NewVenue() {
   return (
     <Layout>
       <Header></Header>
-      <div>
+      <div className={styles.formWrapper}>
         <h1 className={styles.mainTitle}>New Venue</h1>
         <form>
           <Input type="text" label="Name" />
           <Input type="text" label="Description" />
           <Input type="text" label="Media" placeholder="URL" />
-          <Input type="text" label="Price" placeholder="kr" />
-          <Input type="text" label="Max Guests" />
+          <div className={styles.priceGuests}>
+            <Input
+              className={styles.price}
+              type="text"
+              label="Price"
+              placeholder="kr"
+            />
+            <Input className={styles.price} type="text" label="Max Guests" />
+          </div>
           <div className={styles.amenitiesBox}>
-            <span>Amenities</span>
+            <span className={styles.separator}>Amenities</span>
             <Checkbox label="Wifi"></Checkbox>
             <Checkbox label="Parking"></Checkbox>
             <Checkbox label="Breakfast"></Checkbox>
             <Checkbox label="Pets"></Checkbox>
           </div>
-          <span>Location</span>
+          <span className={styles.separator}>Location</span>
           <Input type="text" label="Address" />
           <Input type="text" label="City" />
           <Input type="text" label="Zip" />
